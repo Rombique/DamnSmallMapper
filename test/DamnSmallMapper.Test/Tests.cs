ed72d55e2e5109ce758f69821660fc6e38be77d8
Rombique <roman@bobkov.xyz>
2023-02-19 14:09:34 +0100

    tests

---
+++ test/DamnSmallMapper.Test/Tests.cs
@@ -0,0 +1,61 @@
+using DamnSmallMapper.Test.Entities;
+
+namespace DamnSmallMapper.Test;
+
+public class UnitTest1
+{
+	[Fact]
+	public void MapWithSameProperties()
+	{
+		var client = new Client()
+		{
+			FirstName = "Sergey",
+			LastName = "Bodrov",
+			Age = 35,
+			RegisteredAt = DateTime.Today,
+			IgnoredInClient = "Ignored in Client",
+			IgnoredInPerson = "Ignored in Person"
+		};
+		var person = new Person();
+		person.MapFrom(client);
+		Assert.Equal(client.FirstName, person.FirstName);
+		Assert.Equal(client.LastName, person.Lastname);
+		Assert.Equal(client.Age, person.Age);
+		Assert.Equal(client.RegisteredAt, person.RegisteredAt);
+		Assert.Null(person.IgnoredInClient);
+		Assert.Null(person.IgnoredInPerson);
+	}
+	
+	[Fact]
+	public void MapWithIgnoredProps()
+	{
+		var client = new Client()
+		{
+			FirstName = "Sergey",
+			LastName = "Bodrov",
+			Age = 35
+		};
+		var person = new Person();
+		person.MapFrom(client, nameof(client.Age));
+		Assert.Equal(client.FirstName, person.FirstName);
+		Assert.Equal(client.LastName, person.Lastname);
+		Assert.Equal(0, person.Age);
+	}
+	
+	[Fact]
+	public void MapWithCustomMapProfile()
+	{
+		var customMapProfile = new CustomMapProfile();
+		var client = new Client()
+		{
+			FirstName = "Sergey",
+			LastName = "Bodrov",
+			Age = 35
+		};
+		var person = new Person();
+		person.MapFrom(client, customMapProfile);
+		Assert.Null(person.FirstName);
+		Assert.Equal(client.LastName, person.Lastname);
+		Assert.Equal(client.Age, person.Age);
+	}
+}
